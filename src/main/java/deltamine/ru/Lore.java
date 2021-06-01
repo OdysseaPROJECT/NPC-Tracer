02372222d34de60b5c4bf951c7192982f51db005
Falcion <57592842+Falcion@users.noreply.github.com>
2021-06-01 18:46:52 +0300

    Clear the repository due refactoring of product
    - Final version will be released with development build snapshots.

--- src/main/java/deltamine/ru/Lore.java
+++
@@ -1,9 +0,0 @@
-package deltamine.ru;
-
-public class Lore {
-    public static final String ID = "npctracer";
-    public static final String NAME = "NPC Tracing";
-    public static final String VERSION = "1.0";
-    public static final String CLIENT = "deltamine.ru.proxy.ClientProxy";
-    public static final String COMMON = "deltamine.ru.proxy.CommonProxy";
-}
--- src/main/java/deltamine/ru/handlers/RegistryHandler.java
+++
@@ -1,9 +0,0 @@
-package deltamine.ru.handlers;
-
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-
-public class RegistryHandler {
-    public static void preInitRegistries(FMLPreInitializationEvent event) {
-        ConfigHandler.registerConfig(event);
-    }
-}
